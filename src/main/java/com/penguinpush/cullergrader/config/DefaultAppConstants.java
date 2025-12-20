8f40b7da7e81f0216a2e9d475f0ab8f31e427bc2
Cyprian Zdebski <shipurjan@gmail.com>
2025-12-20 22:20:48 +0100

    update version const

--- src/main/java/com/penguinpush/cullergrader/config/DefaultAppConstants.java
+++ src/main/java/com/penguinpush/cullergrader/config/DefaultAppConstants.java
@@ -1,7 +1,7 @@
 package com.penguinpush.cullergrader.config;
 
 public class DefaultAppConstants {
-    public String VERSION = "1.0.0-fork.4";
+    public String VERSION = "1.0.0-fork.5";
     public boolean DARK_THEME = true;
     public String CACHE_FILE = "cache.json";
     public String LOG_DIRECTORY = "logs";
